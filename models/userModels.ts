a66d540d506dcd2a44fd1e17da781732c1796275
promehedi <admin@promehedi.com>
2023-08-05 15:09:35 +0600

    Hash password with Bun

--- models/userModels.ts
+++ models/userModels.ts
@@ -15,17 +15,24 @@ const userSchema = new mongoose.Schema(
 
 // Match user entered password to hashed password in database
 userSchema.methods.mathPassword = async function (enteredPassword: string) {
-  return await bcrypt.compare(enteredPassword, this.password)
+  //   return await bcrypt.compare(enteredPassword, this.password)
+  return await Bun.password.verify(enteredPassword, this.password)
 }
 
-// Encrypt password using bcrypt
+// Hash password with Bun
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
     next()
   }
 
-  const salt = await bcrypt.genSalt(10)
-  this.password = await bcrypt.hash(this.password, salt)
+  //   const salt = await bcrypt.genSalt(10)
+  //   this.password = await bcrypt.hash(this.password, salt)
+
+  // use bcrypt
+  this.password = await Bun.password.hash(this.password, {
+    algorithm: 'bcrypt',
+    cost: 4, // number between 4-31
+  })
 })
 
 const User = mongoose.model('User', userSchema)
